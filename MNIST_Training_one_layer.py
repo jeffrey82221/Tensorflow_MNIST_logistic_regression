b9d916ae9561f7bdcfa1bc4924f1a9b399a90e2c
Jeffrey Lin <jeffrey82221@gmail.com>
2015-12-29 00:20:58 +0800

    adding Deep Learning

--- MNIST_Training_one_layer.py
+++ MNIST_Training_one_layer.py
@@ -16,11 +16,10 @@ W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
 y_ = tf.placeholder("float", [None,10])
 
-with tf.device('/gpu:0'):
+with tf.device('/cpu:0'):
     y = tf.nn.softmax(tf.matmul(x,W) + b)
     cross_entropy = -tf.reduce_sum(y_*tf.log(y))
     correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-    #accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
     train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 accuracy = tf.reduce_mean(tf.cast(correct_prediction,"float"))
