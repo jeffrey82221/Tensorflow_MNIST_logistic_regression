6a52d4de77067ff3bdf26b4572703c9050f2c190
jeffrey82221 <jeffrey82221@gmail.com>
2015-12-28 20:59:58 +0800

    Removing Reduce Mean and adding counter display

--- MNIST_Training_one_layer.py
+++ MNIST_Training_one_layer.py
@@ -16,13 +16,14 @@ W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
 y_ = tf.placeholder("float", [None,10])
 
-with tf.device('/cpu:0'):
+with tf.device('/gpu:0'):
     y = tf.nn.softmax(tf.matmul(x,W) + b)
     cross_entropy = -tf.reduce_sum(y_*tf.log(y))
     correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    #accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
     train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,"float"))
 init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
@@ -32,4 +33,4 @@ for i in range(10000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
     if i%100==0:
-        print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+        print i,sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
